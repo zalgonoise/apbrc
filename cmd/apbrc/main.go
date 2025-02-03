7acc024abb2b98748facb69188d7901aa3a58605
zalgonoise <mariozalgo@gmail.com>
2025-02-03 23:29:06 +0100

    apbrc: cmd/apbrc: using x/cli for entrypoint management; added check subcommand

--- cmd/apbrc/main.go
+++ cmd/apbrc/main.go
@@ -2,50 +2,89 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log/slog"
-	"os"
-
+	"flag"
+	"github.com/zalgonoise/apbrc/checker"
 	"github.com/zalgonoise/apbrc/config"
 	"github.com/zalgonoise/apbrc/log"
 	"github.com/zalgonoise/apbrc/processor"
 	"github.com/zalgonoise/apbrc/processor/modifiers"
 	"github.com/zalgonoise/apbrc/processor/modifiers/engine"
 	"github.com/zalgonoise/apbrc/processor/modifiers/input"
+	"github.com/zalgonoise/x/cli"
+	"log/slog"
+	"net/http"
+	"time"
 )
 
-func main() {
-	err, code := run()
-	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "error: %v", err)
-	}
+var modes = []string{"apply", "check", ""}
 
-	os.Exit(code)
+func main() {
+	runner := cli.NewRunner("icecloset",
+		cli.WithOneOf(modes...),
+		cli.WithExecutors(map[string]cli.Executor{
+			"":      cli.Executable(ExecApply),
+			"apply": cli.Executable(ExecApply),
+			"check": cli.Executable(ExecCheck),
+		}),
+	)
+
+	cli.Run(runner)
 }
 
-func run() (err error, code int) {
-	ctx := context.Background()
-	cfg, err := config.NewConfig()
+func ExecApply(ctx context.Context, logger *slog.Logger, args []string) (int, error) {
+	cfg, err := config.NewConfig(args)
 	if err != nil {
-		return err, 1
+		return 1, err
 	}
-
-	logger := slog.New(log.NewSpanContextHandler(
-		log.WithHandler(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-			AddSource: true,
-		})),
-		log.WithSpanID(),
-	))
 
 	mods := initMods(cfg, logger)
 
 	proc := processor.ProcessorWithLogs(processor.New(cfg, mods...), logger)
 
 	if err = proc.Run(ctx); err != nil {
-		return err, 1
+		return 1, err
 	}
 
-	return nil, 0
+	return 0, nil
+}
+
+func ExecCheck(ctx context.Context, logger *slog.Logger, args []string) (int, error) {
+	fs := flag.NewFlagSet("apply", flag.ExitOnError)
+
+	timeout := fs.Duration("timeout", 30*time.Second, "how long to wait before a request")
+	freq := fs.Duration("freq", time.Minute, "how frequently should it ping APB servers until it works")
+	count := fs.Int("count", 0, "number of pings to emit")
+	keepPinging := fs.Bool("keep-pinging", false, "keep pinging even if servers are online")
+	failFast := fs.Bool("fail-fast", false, "fail fast")
+
+	if err := fs.Parse(args); err != nil {
+		return 1, err
+	}
+
+	if *count > 0 {
+		logger.InfoContext(ctx, "pinging", slog.Int("num_pings", *count))
+
+		client := &http.Client{Timeout: 30 * time.Second}
+
+		for range *count {
+			checker.CheckAll(ctx, logger, client, *timeout, *failFast)
+		}
+
+		return 0, nil
+	}
+
+	for {
+		logger.InfoContext(ctx, "pinging",
+			slog.Bool("fail-fast", *failFast), slog.Duration("frequency", *freq))
+
+		client := &http.Client{Timeout: 30 * time.Second}
+
+		if ok := checker.CheckAll(ctx, logger, client, *timeout, *failFast); ok && !*keepPinging {
+			return 0, nil
+		}
+
+		time.Sleep(*freq)
+	}
 }
 
 func initMods(cfg *config.Config, logger log.Logger) []processor.Applier {
